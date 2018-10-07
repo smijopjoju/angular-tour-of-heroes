11576d8d50ac830a22d4d45f5016e67dafc72224
Joju <jojusmi@ent.bhicorp.com>
2018-10-07 09:04:43 +0530

    First Commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROS : Hero[]=[
+    { id: 1, name: 'smijo'},
+    { id: 2, name: 'smiji'},
+    { id: 3, name: 'smilja'},
+    { id: 4, name: 'Jeesmary'},
+    { id: 5, name: 'Jwala Jees'},
+    { id: 6, name: 'Joju'},
+    { id: 7, name: 'Lisy'},
+    { id: 8, name: 'Veronica'},
+    { id: 9, name: 'Vareed'}
+];
